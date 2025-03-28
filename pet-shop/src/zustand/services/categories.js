a6b07ebf662b01d45c996396e95d878451a7763e
Heorhii <91286488+GertSer@users.noreply.github.com>
2025-03-28 17:13:25 +0100

    Add console logs for successful category data

--- pet-shop/src/zustand/services/categories.js
+++ pet-shop/src/zustand/services/categories.js
@@ -5,10 +5,11 @@ const API_URL = "https://pet-shop-backend-0fzb.onrender.com/categories";
 export const get = async () => {
   try {
     const response = await axios.get(`${API_URL}/all`);
+    console.log("Category data fetched successfully:", response.data);
     return response.data;
   } catch (error) {
     throw new Error(
-      error.response ? error.response.data.message : error.message
+      error.response ? error.response.data.message : "Network error"
     );
   }
 };
@@ -16,10 +17,11 @@ export const get = async () => {
 export const getById = async (id) => {
   try {
     const response = await axios.get(`${API_URL}/${id}`);
+    console.log("Category data fetched successfully:", response.data);
     return response.data;
   } catch (error) {
     throw new Error(
-      error.response ? error.response.data.message : error.message
+      error.response ? error.response.data.message : "Network error"
     );
   }
 };
