47bba27bc71e306001da31c7eb0d3d41d270fd70
Heorhii <91286488+GertSer@users.noreply.github.com>
2025-04-02 17:51:23 +0200

    Refactor Products and Home components to integrate LoadingErrorHandler for improved loading state management

--- pet-shop/src/components/Products/Products.jsx
+++ pet-shop/src/components/Products/Products.jsx
@@ -9,9 +9,11 @@ import SectionTitle from "../SectionTitle/SectionTitle";
 import NavigationButton from "../NavButton/NavButton";
 import { useTheme } from "@mui/material/styles";
 import { NavLink } from "react-router-dom";
+import LoadingErrorHandler from "../../components/LoadingErrorHandler/LoadingErrorHandler";
+
 export default function Products() {
   const [isAnimating, setIsAnimating] = useState(false);
-  const { products } = useProductStore();
+  const { products, loading, error } = useProductStore();
   const [activeIndProducts, setActiveIndProducts] = useState(0);
   const [isCardHovered, setCardHovered] = useState({});
   const [addedToCart, setAddedToCart] = useState({});
@@ -50,114 +52,122 @@ export default function Products() {
 
   return (
     <section className={styles.coupleSection}>
-      <div className={styles.coupleContainer}>
-        <SectionTitle content="Sale" />
-        <span className={styles.coupleLine}></span>
-        <NavigationButton text="All sales" route="/allsales" />
-      </div>
+      <LoadingErrorHandler loading={loading} error={error} />
+      {!loading && !error && (
+        <>
+          <div className={styles.coupleContainer}>
+            <SectionTitle content="Sale" />
+            <span className={styles.coupleLine}></span>
+            <NavigationButton text="All sales" route="/allsales" />
+          </div>
 
-      <div
-        className={`${styles.coupleSliderWrapper} ${
-          isAnimating ? styles.animateSlide : ""
-        }`}
-      >
-        <KeyboardArrowLeft sx={{ cursor: "pointer" }} onClick={prevSlide} />
+          <div
+            className={`${styles.coupleSliderWrapper} ${
+              isAnimating ? styles.animateSlide : ""
+            }`}
+          >
+            <KeyboardArrowLeft sx={{ cursor: "pointer" }} onClick={prevSlide} />
 
-        <ul className={styles.productsRenderList}>
-          {filteredProducts
-            .slice(activeIndProducts, activeIndProducts + visibleProSlide)
-            .concat(
-              filteredProducts.slice(
-                0,
-                Math.max(
-                  0,
-                  visibleProSlide -
-                    (filteredProducts.length - activeIndProducts)
+            <ul className={styles.productsRenderList}>
+              {filteredProducts
+                .slice(activeIndProducts, activeIndProducts + visibleProSlide)
+                .concat(
+                  filteredProducts.slice(
+                    0,
+                    Math.max(
+                      0,
+                      visibleProSlide -
+                        (filteredProducts.length - activeIndProducts)
+                    )
+                  )
                 )
-              )
-            )
-            .map((product) => (
-              <NavLink to={`/products/${product.id}`} key={product.id}>
-                <li
-                  className={styles.productsRenderItem}
-                  key={product.id}
-                  onMouseEnter={() =>
-                    setCardHovered((prev) => ({
-                      ...prev,
-                      [product.id]: true,
-                    }))
-                  }
-                  onMouseLeave={() =>
-                    setCardHovered((prev) => ({
-                      ...prev,
-                      [product.id]: false,
-                    }))
-                  }
-                >
-                  <span className={styles.discountBorder}>
-                    -
-                    {Math.floor(
-                      100 - (product.discont_price * 100) / product.price
-                    )}
-                    %
-                  </span>
-
-                  <button
-                    className={styles.cartButton}
-                    onClick={(event) => {
-                      event.stopPropagation();
-                      push(product);
-                      setAddedToCart((prev) => ({
-                        ...prev,
-                        [product.id]: true,
-                      }));
-                      setTimeout(() => {
-                        setAddedToCart((prev) => ({
+                .map((product) => (
+                  <NavLink to={`/products/${product.id}`} key={product.id}>
+                    <li
+                      className={styles.productsRenderItem}
+                      key={product.id}
+                      onMouseEnter={() =>
+                        setCardHovered((prev) => ({
+                          ...prev,
+                          [product.id]: true,
+                        }))
+                      }
+                      onMouseLeave={() =>
+                        setCardHovered((prev) => ({
                           ...prev,
                           [product.id]: false,
-                        }));
-                      }, 3000);
-                    }}
-                    style={{
-                      opacity: isCardHovered[product.id] ? 1 : 0,
-                      backgroundColor: addedToCart[product.id]
-                        ? "black"
-                        : "#0d50ff",
-                    }}
-                  >
-                    {addedToCart[product.id] ? "Added" : "Add to basket"}
-                  </button>
-
-                  <img
-                    className={styles.productsRenderImage}
-                    src={`https://pet-shop-backend-0fzb.onrender.com${product.image}`}
-                    alt={product.title}
-                  />
-                  <div className={styles.productsContent}>
-                    <p
-                      className={styles.productsTitle}
-                      style={{ color: theme.palette.text.primary }}
+                        }))
+                      }
                     >
-                      {product.title}
-                    </p>
+                      <span className={styles.discountBorder}>
+                        -
+                        {Math.floor(
+                          100 - (product.discont_price * 100) / product.price
+                        )}
+                        %
+                      </span>
 
-                    <span
-                      className={styles.primaryPrice}
-                      style={{ color: theme.palette.text.primary }}
-                    >
-                      ${product.discont_price}
-                    </span>
-                    <span className={styles.secondaryPrice}>
-                      ${product.price}
-                    </span>
-                  </div>
-                </li>
-              </NavLink>
-            ))}
-        </ul>
+                      <button
+                        className={styles.cartButton}
+                        onClick={(event) => {
+                          event.stopPropagation();
+                          push(product);
+                          setAddedToCart((prev) => ({
+                            ...prev,
+                            [product.id]: true,
+                          }));
+                          setTimeout(() => {
+                            setAddedToCart((prev) => ({
+                              ...prev,
+                              [product.id]: false,
+                            }));
+                          }, 3000);
+                        }}
+                        style={{
+                          opacity: isCardHovered[product.id] ? 1 : 0,
+                          backgroundColor: addedToCart[product.id]
+                            ? "black"
+                            : "#0d50ff",
+                        }}
+                      >
+                        {addedToCart[product.id] ? "Added" : "Add to basket"}
+                      </button>
 
-        <KeyboardArrowRight sx={{ cursor: "pointer" }} onClick={nextSlide} />
-      </div>
+                      <img
+                        className={styles.productsRenderImage}
+                        src={`https://pet-shop-backend-0fzb.onrender.com${product.image}`}
+                        alt={product.title}
+                      />
+                      <div className={styles.productsContent}>
+                        <p
+                          className={styles.productsTitle}
+                          style={{ color: theme.palette.text.primary }}
+                        >
+                          {product.title}
+                        </p>
+
+                        <span
+                          className={styles.primaryPrice}
+                          style={{ color: theme.palette.text.primary }}
+                        >
+                          ${product.discont_price}
+                        </span>
+                        <span className={styles.secondaryPrice}>
+                          ${product.price}
+                        </span>
+                      </div>
+                    </li>
+                  </NavLink>
+                ))}
+            </ul>
+
+            <KeyboardArrowRight
+              sx={{ cursor: "pointer" }}
+              onClick={nextSlide}
+            />
+          </div>
+        </>
+      )}
     </section>
   );
 }
--- pet-shop/src/pages/Home/Home.jsx
+++ pet-shop/src/pages/Home/Home.jsx
@@ -2,23 +2,14 @@ import Banner from "../../components/Banner/Banner";
 import Categories from "../../components/Categories/Categories";
 import DiscountForm from "../../components/DiscountForm/DiscountForm";
 import Products from "../../components/Products/Products";
-import LoadingErrorHandler from "../../components/LoadingErrorHandler/LoadingErrorHandler";
-import useCategoryStore from "../../zustand/stores/categories";
 
 export default function Home() {
-  const { loading, error } = useCategoryStore();
-
   return (
     <main>
-      <LoadingErrorHandler loading={loading} error={error} />
-      {!loading && !error && (
-        <>
-          <Banner />
-          <Categories />
-          <DiscountForm />
-          <Products />
-        </>
-      )}
+      <Banner />
+      <Categories />
+      <DiscountForm />
+      <Products />
     </main>
   );
 }
